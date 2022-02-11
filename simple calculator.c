e7ecf0594b0c50e8da0a6fb485d59b704e47bcab
Ombasa-ferdinand <99478277+Ombasa-ferdinand@users.noreply.github.com>
2022-02-11 14:04:42 +0300

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,31 @@
+/*
+simple calculator
+by Ombasa Ferdinand
+Feb 2022
+MIT license
+C89 Compiler
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+int main()
+{
+    //Variable Declaration
+    int num1,num2,sum,diff,product;
+    float quotient;
+    printf("Simple Calculator!\n");
+    //capture inputs
+    printf("Enter two values: ");
+    scanf("%d%d",&num1, &num2);
+    //computation
+    sum = num1 + num2;
+    diff = num1 - num2;
+    product = num1 * num2;
+    quotient = (float)num1 / num2;
+    //output
+    printf("%d+%d=%d\n",num1,num2,sum);
+    printf("%d-%d=%d\n",num1,num2,diff);
+    printf("%d*%d=%d\n",num1,num2,product);
+    printf("%d/%d=%f\n",num1,num2,quotient);
+    return 0;
+}
